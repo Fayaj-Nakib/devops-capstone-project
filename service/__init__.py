e5b20d260275f68162763d14dd1388f56f01e393
Fayaj-Nakib <nakibfayaj99@gmail.com>
2026-05-10 23:11:52 -0400

    Fix: Disable force_https in Talisman for testing

--- service/__init__.py
+++ service/__init__.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 # Initialize Talisman Security Headers
-talisman = Talisman(app)  # <--- Add this
+talisman = Talisman(app, force_https=False)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
